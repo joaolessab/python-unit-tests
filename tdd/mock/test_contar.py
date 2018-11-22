ff88103235d008f155f6d989fb46b9b521b4f4a4
João Vitor Lessa <joaovitorlessa@gmail.com>
2018-11-22 15:26:29 -0200

    Mocking functions

--- tdd/mock/test_contar.py
+++ tdd/mock/test_contar.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import Mock, patch
 import contar
 
 class TestContar(unittest.TestCase):
@@ -8,4 +9,13 @@ class TestContar(unittest.TestCase):
         tamanho = contar.contar_arquivo()
         
         # Assert
-        self.assertEqual(3, tamanho)
+        self.assertEqual(16, tamanho)
+
+    @patch("builtins.open")
+    def teste_deve_retornar_tamanho_mock(self, open_mock):
+        # Arrange
+        open_mock.return_value.read.return_value = "qualquer coisa"
+
+        tamanho = contar.contar_arquivo()
+
+        self.assertEqual(14, tamanho)
